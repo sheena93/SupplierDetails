dc28a7f82a39c31a0891c0eaeee0ae4ef4b256ff
703237284 <703237284@genpact.com>
2020-05-11 20:53:13 +0530

    removed unused code

--- src/SupplierSettingsPage/SupplierSettingsSections.tsx
+++ src/SupplierSettingsPage/SupplierSettingsSections.tsx
@@ -13,61 +13,19 @@ import { supplierInitialMockData } from "./SupplierInitialMockData";
 import {
   ReserveSettings,
   SupplierSetting,
-  PayLoadAPRSettings,
   defaultAprSetting
 } from "./Supplier.schema";
 
 const useStyles = makeStyles<Theme>((theme) => ({
-  supplierContainer: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
   baseContainer: {
     borderRadius: 5,
   },
   supplierNameContainer: {
     padding: theme.spacing(2),
   },
-  bottomContainer: {
-    backgroundColor: Liquidity.colors.greys.background,
-    display: "flex",
-    flex: 1,
-    flexDirection: "row",
-    padding: 8,
-    margin: 2,
-  },
-  labels: {
-    padding: theme.spacing(2),
-  },
   papergap: {
     padding: theme.spacing(2),
-  },
-  textField: {
-    flexGrow: 1,
-    paddingLeft: theme.spacing(1),
-    "& .MuiOutlinedInput-input": {
-      paddingTop: theme.spacing(1),
-      paddingBottom: theme.spacing(1),
-    },
-    width: "44ch",
-    margin: theme.spacing(1),
-    display: "grid",
-  },
-  saveButton: {
-    backgroundColor: "#008000",
-    padding: theme.spacing(1, 2.5),
-    [theme.breakpoints.up("sm")]: {
-      padding: theme.spacing(1, 3),
-      margin: theme.spacing(2),
-    },
-  },
-  sections: {
-    paddingLeft: theme.spacing(1),
-    display: "-webkit-box",
-  },
-  radiodisplay: {
-    display: "flow-root",
-  },
+  }
 }));
 
 const SupplierSettingsSections: React.FC = () => {
