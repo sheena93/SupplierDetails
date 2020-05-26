07dc2d9c9ceab6f3adbc1f462faa3964471e3b26
703237284 <703237284@genpact.com>
2020-05-26 22:07:53 +0530

    review comments incorporated

--- src/Filter/FilterDrawer.tsx
+++ src/Filter/FilterDrawer.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode } from 'react';
 import { Box, Button, Divider, Drawer, Grid, TypeBase } from '@c2fo/components';
 
-import {useStyles} from "./FilterWrapper.style";
+import { useStyles } from './FIlterWrapper.style';
+// import { T } from '../../../i18n';
 
 const TESTID_ROOT = 'filterDrawer';
 export const testIds = {
@@ -19,79 +20,85 @@ type FilterDrawerProps = {
 };
 
 export const FilterDrawer: React.FC<FilterDrawerProps & ReactNode> = ({
-                                                                        children,
-                                                                        showDrawer,
-                                                                        applyFilter,
-                                                                        resetFilter,
-                                                                        cancelFilter,
-                                                                      }) => {
+  children,
+  showDrawer,
+  applyFilter,
+  resetFilter,
+  cancelFilter,
+}) => {
   const classes = useStyles();
 
   return (
-      <Drawer
-          data-testid={testIds.drawer}
-          anchor="right"
-          open={showDrawer}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          ModalProps={
-            cancelFilter
-                ? {
-                  onBackdropClick: () => {
-                    cancelFilter();
-                  },
-                }
-                : {}
-          }
-      >
-        <div className={classes.drawer}>
-          <Box className={classes.addFiltersTitle}>
-            <TypeBase isEmphasis>
-              {/* todo add translation */}
-              ADD FILTERS
-            </TypeBase>
-          </Box>
-          <Divider />
-          <div className={classes.Drawerchildren}> {children}
-          <Button
-              className={classes.resetFilters}
-              data-testid={testIds.resetButton}
+    <Drawer
+      data-testid={testIds.drawer}
+      anchor="right"
+      open={true}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      ModalProps={
+        cancelFilter
+          ? {
+              onBackdropClick: () => {
+                cancelFilter();
+              },
+            }
+          : {}
+      }
+    >
+      <Grid className={classes.drawer}>
+        <Box className={classes.addFiltersTitle}>
+          <TypeBase isEmphasis>
+            {/* todo add translation */}
+            {/* <T k="core.addFilters">ADD FILTERS</T> */}
+            Add Filters
+          </TypeBase>
+        </Box>
+        <Divider />
+        <div className={classes.drawerchildren}> {children} 
+        <Grid item sm="auto" xs={12}>
+        <Button
+          className={classes.resetFilters}
+          data-testid={testIds.resetButton}
+          variant="text"
+          color="secondary"
+          onClick={resetFilter}
+        >
+          {/* todo add translation */}
+          {/* <T k="core.resetFilter">Reset Filters</T> */}
+          Reset Filters
+        </Button>
+        </Grid>
+        </div>
+        <div  className={classes.spacer}/>
+        <Grid container className={classes.filterButtons} spacing={1}>
+          <Grid item sm="auto" xs={12}>
+            <Button
+              data-testid={testIds.cancelButton}
+              className={classes.cancelButton}
+              size="large"
               variant="text"
               color="secondary"
-              onClick={resetFilter}
-          >
-            {/* todo add translation */}
-            Reset Filters
-          </Button>
-          </div>
-          <div  className={classes.spacer}></div>
-          <Grid container className={classes.filterButtons} spacing={1}>
-            <Grid item sm="auto" xs={12}>
-              <Button
-                  data-testid={testIds.cancelButton}
-                  className={classes.cancelButton}
-                  size="large"
-                  variant="text"
-                  color="secondary"
-                  onClick={cancelFilter}
-              >
-                Cancel
-              </Button>
-            </Grid>
-            <Grid item sm="auto" xs={12}>
-              <Button
-                  data-testid={testIds.applyButton}
-                  className={classes.applyButton}
-                  size="large"
-                  variant="contained"
-                  onClick={applyFilter}
-              >
-                Apply
-              </Button>
-            </Grid>
+              onClick={cancelFilter}
+            >
+              {/* <T k="core.cancel">Cancel</T> */}
+              Cancel
+            </Button>
           </Grid>
-        </div>
-      </Drawer>
+          <Grid item sm="auto" xs={12}>
+            <Button
+              data-testid={testIds.applyButton}
+              className={classes.applyButton}
+              size="large"
+              variant="contained"
+              onClick={applyFilter}
+            >
+              {/* <T k="core.apply">Apply</T> */}
+              Apply
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Drawer>
   );
 };
